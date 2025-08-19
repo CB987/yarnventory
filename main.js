d9b1f226a92eb8a2d81dfd3fee0e6e7877865ebb
C Barton <cbarto8@emory.edu>
2025-08-19 17:43:11 -0400

    up to form validation in the modal. turns out you need the button type submit in order to have an event listener on it.

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
+});
+
+let formValidation = () => {
+  if (textInput.value === "") {
+    msg.innerHTML = "oops, you didn't enter anything";
+    console.log("complete and utter failure");
+  } else {
+    console.log("stirring success");
+    msg.innerHTML = "";
+  }
+};
 
 // need to make the submit button work by hitting enter from the textarea
 // field for timestamp for any updates, or versions? notes field that appends, with. timestamp?
